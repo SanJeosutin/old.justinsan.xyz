7d9c835aa423baab71e13b1847a23990a1c75393
Jury Quackinton <102561570@student.swin.edu.au>
2020-12-19 14:09:24 +1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?
+/*
+ * File name:   index.php
+ * Author   :   Justin San
+ * -=-=-=-=-=-=-=-=-=-=-=-=
+ * Created  :   19/12/2020
+ * Updated  :   19/12/2020
+*/
+?>
+	<div id="partcileBackground">
+		<div id="topLayer">
+			<!--MAIN CONTENT-->
+			<div class="row h-100">
+				<div class="col-sm-12 my-auto">
+					<div class="card-body index-font">
+						<h1 class="card-title title text-center">San Justin</h1>
+						<ul class="nav nav-tabs card-header-tabs justify-content-center">
+							<li class="nav-item">
+								<a class="nav-link bg-yellowgreen" href="projects.php">Projects</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link bg-yellowgreen" href="#About">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link bg-yellowgreen" href="#Designs">Designs</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include('./incs/header.inc'); ?>
+
+<?php include('./incs/footer.inc'); ?>
---
+++ projects.php
@@ -0,0 +1,24 @@
+<?
+/*
+ * File name:   project.php
+ * Author   :   Justin San
+ * -=-=-=-=-=-=-=-=-=-=-=-=
+ * Created  :   19/12/2020
+ * Updated  :   19/12/2020
+*/
+?>
+
+<?php 
+include('./incs/header.inc'); 
+include('./incs/navbar.inc'); 
+?>
+<div class="jumbotron jumbotron-fluid bg-yellowgreen">
+  <div class="container">
+    <h1 class="display-4">Few of my projects</h1>
+    <p class="lead">
+        For the past couple of years, I've been working on a few side projects 
+        <em>(Weather it's a small ones or a big ones. All is in here!)</em> to build up my portfolio.
+        You can find them bellow. Happy browsing!</p>
+  </div>
+</div>
+<?php include('./incs/footer.inc'); ?>
